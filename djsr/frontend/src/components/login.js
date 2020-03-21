c557a89d9637f76743d1ecb854c5cd613f2e4d57
paul <choppin.pgmail.com>
2020-03-21 14:01:11 +0100

    added components for loggin

--- djsr/frontend/src/components/login.js
+++ djsr/frontend/src/components/login.js
@@ -3,6 +3,19 @@ import React, { Component } from "react";
 class Login extends Component{
     constructor(props){
         super(props);
+        this.state = {username: "", password: ""};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A username and password was submitted: ' + this.state.username + " " + this.state.password);
+        event.preventDefault();
     }
 
     render() {
